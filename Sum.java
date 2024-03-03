fe53e79fd71b2e5b8f2e7b64bdeff8dd1d270510
Pawan Singh <161993408+PawanSingh102@users.noreply.github.com>
2024-03-03 09:04:38 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Sum 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter 1st number : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter 2nd number : ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Sum is " + (num1 + num2));
+
+        sc.close();
+    }
+    
+}
